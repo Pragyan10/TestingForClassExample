053da4e36c29b5c0dc3f0557f1c76a84b0bf6b3d
Pragyan10 <pragyankc1@gmail.com>
2026-03-16 18:53:12 -0400

    this is my first commit for the repository

---
+++ src/User.java
@@ -0,0 +1,14 @@
+public class User {
+
+    private String name;
+
+    public User(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
