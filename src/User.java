3a59f27600c5765fabf11c57d82c8474a18f185a
Pragyan10 <pragyankc1@gmail.com>
2026-03-18 12:59:05 -0500

    made a new attribute

--- src/User.java
+++ src/User.java
@@ -1,9 +1,11 @@
 public class User {
 
     private String name;
+    private String address;
 
-    public User(String name){
+    public User(String name, String address){
         this.name = name;
+        this.address = address;
     }
 
     @Override
