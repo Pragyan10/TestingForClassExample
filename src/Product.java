a8e7183e1b314065ac27b10951e8d0cb82cc0490
Pragyan10 <pragyankc1@gmail.com>
2026-03-19 11:27:55 -0500

    added product file

---
+++ src/Product.java
@@ -0,0 +1,11 @@
+public class Product{
+
+    private String productName;
+    private String productPrice;
+
+    public Product(String productName, String productPrice){
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+}
+
